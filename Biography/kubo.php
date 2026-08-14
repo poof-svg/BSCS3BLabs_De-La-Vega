81cac6311357b45b01cca6fefea06428a886dd83
Phloem <phloemd@gmail.com>
2026-08-14 15:58:35 +0800

    Done Task

---
+++ Biography/kubo.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php
+            $pageTitle = "Kubo";
+        ?>
+        <title>
+            <?php echo $pageTitle; ?>
+        </title>
+    </head>
+    <body>
+       <?php
+         $pageHeader = "Love Still Kubo Series";
+         echo "<center><h1>" . $pageHeader . "</h1></center>";
+        ?>
+
+
+        <p>
+            <br>
+            <center>
+            <img src="https://prod-apac-r2.popmart.com/cdn-cgi/image/width=3840%2Cformat=webp/https://prod-eurasian-res.popmart.com/default/ef271ca081de32298cddf51c7121943d_____1200x1200.jpg" 
+                 alt="Love Still Kubo Series Collectibles" 
+                 width="400" 
+                 height="400">
+            </center>
+            <br>
+            <center>
+                Love isn't always loud. 
+            Sometimes it's just remembering the little things 🩷.
+            </center>
+
+             <?php
+            $pageHeader = "The Lore:";
+            echo "<h1>" . $pageHeader . "</h1>";
+            ?>
+            <br>
+            
+            The KUBO <b>"Love, Still"</b> series by Pop Mart explores the emotional imprint left by people who changed your life but did not stay. Rather than focusing on heartbreak, the lore centers on quiet nostalgia, unspoken words, and memories frozen in time. The core message is that love remains and evolves even after people or chapters depart, using each figure to represent a <i>lingering emotion, a past version of the self, or a soft, unvoiced confession</i>. <b>"Ashes of a Secret" </b> features a figure holding a burned letter by a mailbox overflowing with hearts to represent hidden confessions, while <b> "Butterflies In My Stomach"</b> uses a skeleton X-ray filled with butterflies to symbolize nervous, brand-new affection. Other figures like <b>"Love is Blind," "Build Our Home," and "Dreams of You" </b> capture the unseeing nature of infatuation, the longing for emotional safety, and late-night thoughts dedicated to a memory, culminating in the rare <b> "Secret Folded Wishes"</b> figure which embodies your deepest withheld desires.
+
+
+
+            <br>
+            <br>
+
+
+
+
+
+         <footer>
+        <p>
+            <center>
+            <strong>Other Pages:</strong> 
+            <a href="harryPotter.php">Harry Potter</a> | 
+            <a href="hirono.php">Hirono</a>
+            🙂‍↕️
+            </center>
+        </p>
+    </footer>
+    </body>
+</html>
+
+
+
